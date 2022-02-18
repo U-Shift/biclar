b3c4ef610af701d555cf7ab1b39bd1b7798f8c2c
Robin Lovelace <rob00x@gmail.com>
2022-02-18 00:31:17 +0000

    Add jittering code

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
 # bicla-R
 
-A place to store code and (in the releases) data for estimating cycling potential and influencing policy.
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of bicla-R is to store code and (in the releases) data for
+estimating cycling potential and influencing policy.
+
+# Input data
+
+The key datasets are as follows:
+
+-   TRIPS_original.Rds, the original OD dataset
+
+The OD datasets, before and after jittering, are shown below.
+
+<img src="README_files/figure-gfm/jitteredoverview-1.png" width="50%" /><img src="README_files/figure-gfm/jitteredoverview-2.png" width="50%" />
--- code/test-code/jittering.R
+++ code/test-code/jittering.R
@@ -1,10 +1,55 @@
 # Aim: test out Jittering in study region
 remotes::install_github("atumworld/odr")
 library(tidyverse)
+library(tmap)
+tmap_mode("view")
 
 od_all = readRDS("TRIPSmode_freguesias.Rds")
 zones = readRDS("FREGUESIASgeo.Rds")
 od_all_sf = od::od_to_sf(od_all, zones)
-plot(od_all_sf)
+# plot(od_all_sf)
+od_all_sf_top = od_all_sf %>% 
+  filter(DICOFREor11 != DICOFREde11) %>% 
+  top_n(n = 100, wt = Total)
 
-osm_data_region = osmextract::oe_get_network(place = "Portugal", mode = "cycling", )
+# osm_data_national = osmextract::oe_get_network(place = "Portugal", mode = "cycling", force_download = TRUE)
+# nrow(osm_data_national) # 1 million rows, 1106127 (2022-02)
+# osm_data_region = osm_data_national[zones, ]
+# nrow(osm_data_region) # 142201
+# saveRDS(osm_data_region, "osm_data_region.Rds")
+# piggyback::pb_upload("osm_data_region.Rds")
+osm_data_region = readRDS("osm_data_region.Rds")
+osm_data_region_top = osm_data_region %>% 
+  sample_n(500)
+
+tm_shape(zones) +
+  tm_borders() +
+  tm_shape(osm_data_region_top) +
+  tm_lines("highway")
+
+summary(od_all$Total)
+set.seed(42)
+od_all_jittered = odr::odr_jitter(
+  od = od_all,
+  zones = zones,
+  subpoints = osm_data_region,
+  disaggregation_key = "Total",
+  disaggregation_threshold = 50
+)
+nrow(od_all_jittered) # 110008
+saveRDS(od_all_jittered, "od_all_jittered_50.Rds")
+piggyback::pb_upload("od_all_jittered_50.Rds")
+
+# case study zones
+small_netowrk = sf::st_transform(slopes::lisbon_road_network, 4326)
+lisbon_central = zones[small_netowrk, ]
+qtm(lisbon_central)
+
+od_jittered_central = od_all_jittered %>%
+  filter(DICOFREde11 %in% lisbon_central$Dicofre)
+qtm(od_jittered_central)
+
+od_jittered_central = od_all_jittered[lisbon_central, ]
+od_jittered_top = od_jittered_central %>% 
+  sample_n(size = 1000)
+qtm(od_jittered_top)
