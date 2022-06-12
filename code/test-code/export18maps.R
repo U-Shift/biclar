40446569cf1b9d277e7f07a3a72504640a2d7c36
temospena <temospena@gmail.com>
2022-06-12 16:18:59 +0100

    fast routes 18export

--- code/test-code/export18maps.R
+++ code/test-code/export18maps.R
@@ -7,33 +7,33 @@ library(biclar)
 MUNICIPIOSgeo = readRDS(url("https://github.com/U-Shift/biclar/releases/download/0.0.1/MUNICIPIOSgeo.Rds"))
 MUNICIPIOS = MUNICIPIOSgeo$Concelho
 
-REDEregion = readRDS("rnet_enmac_quietest_full.Rds")
-# REDEregion = readRDS("rnet_enmac_fastest_full.Rds")
+# REDEregion = readRDS("rnet_enmac_quietest_full.Rds") #quiet
+REDEregion = readRDS("rnet_enmac_fastest_full.Rds") #fast
 
 scenario = "enmac10" #baseline, enmac4, enmac10
-route = "quiet" #quiet, fast
+route = "fast" #quiet, fast
 
 #Quiet, Baseline - DONE
 #Quiet, ENMAC4 - DONE
 #Quiet, ENMAC10 - DONE
-#Fast, Baseline
-#Fast, ENMAC4
+#Fast, Baseline - DONE
+#Fast, ENMAC4 - DONE
 #Fast, ENMAC10
 
 for(i in 1:length(MUNICIPIOS)){
-  mun = MUNICIPIOS[i]
-  BUFFER = MUNICIPIOSgeo %>% filter(Concelho == mun) %>% st_buffer(500)
+  mun = MUNICIPIOS[i] #test with Lisbon 6
+  BUFFER = MUNICIPIOSgeo %>% filter(Concelho == mun) %>% st_buffer(500) #maybe reduce to 200? 
   REDE = REDEregion %>% st_filter(BUFFER)
-  REDE = REDE %>% filter(ENMAC10 >= quantile(REDE$ENMAC10, 0.60)) #try other slices, change here scenario
+  REDE = REDE %>% filter(ENMAC10 >= quantile(REDE$ENMAC10, 0.70)) #0.6 for quiet, 0.7 for fast, change here scenario
   
   #make and export 18 maps
   m = tm_rnet(
     REDE,
     lwd = "ENMAC10", #Baseline, ENMAC4, ENMAC10
     col = "Quietness",
-    palette = "-mako", # "mako", "reds" - reds for fastest, mako for quietest
-    scale = 12,
-    lwd_multiplier = 12 #15 used in region
+    palette = "-linear_yl_rd_bk", # "mako", "linear_yl_rd_bk" - linear_yl_rd_bk for fastest, mako for quietest (tried reds, rocket, burg)
+    scale = 15,
+    lwd_multiplier = 15 #15 used in region and fast, 12 for quiet
   )
   
   mun = tolower(mun)
