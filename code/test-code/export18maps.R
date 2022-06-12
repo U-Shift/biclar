24537ed29c1692181e6ded96995e43aeaa9f6f48
temospena <temospena@gmail.com>
2022-06-12 17:40:09 +0100

    vfxira
    it was stactic before

--- code/test-code/export18maps.R
+++ code/test-code/export18maps.R
@@ -20,11 +20,11 @@ route = "fast" #quiet, fast
 #Fast, ENMAC4 - DONE
 #Fast, ENMAC10
 
-for(i in 1:length(MUNICIPIOS)){
+for(i in 1:(length(MUNICIPIOS)-1)){
   mun = MUNICIPIOS[i] #test with Lisbon 6
   BUFFER = MUNICIPIOSgeo %>% filter(Concelho == mun) %>% st_buffer(500) #maybe reduce to 200? 
   REDE = REDEregion %>% st_filter(BUFFER)
-  REDE = REDE %>% filter(ENMAC10 >= quantile(REDE$ENMAC10, 0.70)) #0.6 for quiet, 0.7 for fast, change here scenario
+  REDE = REDE %>% filter(ENMAC10 >= quantile(REDE$ENMAC10, 0.70)) #0.6 for quiet, 0.7 for fast, change here scenario. (Barreiro should be 0.65)
   
   #make and export 18 maps
   m = tm_rnet(
@@ -42,4 +42,28 @@ for(i in 1:length(MUNICIPIOS)){
 }
 
 
-# Manually open «vila franca xira html, save as, vfxira, and delete the previous ones
+## Manually save  vila franca xira , as, vfxira ######
+
+# REDEregion = readRDS("rnet_enmac_quietest_full.Rds") #quiet
+REDEregion = readRDS("rnet_enmac_fastest_full.Rds") #fast
+
+scenario = "baseline" #baseline, enmac4, enmac10
+route = "fast" #quiet, fast
+
+mun = MUNICIPIOS[18] #Vila Franca de Xira
+BUFFER = MUNICIPIOSgeo %>% filter(Concelho == mun) %>% st_buffer(500) #maybe reduce to 200? 
+REDE = REDEregion %>% st_filter(BUFFER)
+REDE = REDE %>% filter(Baseline >= quantile(REDE$Baseline, 0.70)) #0.6 for quiet, 0.7 for fast, change here scenario. (Barreiro should be 0.65)
+
+#make and export 18 maps
+m = tm_rnet(
+  REDE,
+  lwd = "Baseline", #Baseline, ENMAC4, ENMAC10
+  col = "Quietness",
+  palette = "-linear_yl_rd_bk", # "mako", "linear_yl_rd_bk" - linear_yl_rd_bk for fastest, mako for quietest (tried reds, rocket, burg)
+  scale = 15,
+  lwd_multiplier = 15 #15 used in region and fast, 12 for quiet
+)
+
+htmlwidgets::saveWidget(m, paste0("pkgdown/assets/vfxira_", scenario, "_", route, ".html")) #Só com VFXIRA
+
