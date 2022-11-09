5f1b516e409625c986035a82a05acf96634720c2
temospena <temospena@gmail.com>
2022-11-09 22:02:53 +0000

    ebike scenario

--- code/test-code/r5r_filter_overline_routes.R
+++ code/test-code/r5r_filter_overline_routes.R
@@ -12,19 +12,24 @@ table(routes_r5r_100jit_lts4__ferry_elev$mode)
 # 54384    2011 
 summary(routes_r5r_100jit_lts4__ferry_elev$distance)
 
-
 routes_ferry4_filtered = routes_r5r_100jit_lts4__ferry_elev %>%
   filter(mode == "BICYCLE") %>%
   st_drop_geometry() %>%
   group_by(id) %>% 
   summarise(distance = distance)
-summary(routes_ferry4_filtered$distance) #75% less than 9000m in total
-routes_ferry4_filtered = routes_ferry4_filtered %>% filter(distance <= 9000)
+summary(routes_ferry4_filtered$distance) #50% less than 5000m, 75% less than 9000m in total
+# routes_ferry4_filtered = routes_ferry4_filtered %>% filter(distance <= 9000)
+routes_ferry4_filtered = routes_ferry4_filtered %>% filter(distance <= 5000) #fazer até 5km para os cenarios 4 e 10
+
 
 routes_ferry4_filtered = routes_r5r_100jit_lts4__ferry_elev %>% filter(id %in% routes_ferry4_filtered$id)
 table(routes_ferry4_filtered$mode)
-# 41k trips by bike
-colnames(routes_ferry4_filtered)
+# BICYCLE   FERRY 
+# 28197    1441 
+# 2.410k    117k
+# 2.233k (2  pernas da mesma viagem, first & last mile) 
+
+# sum(routes_ferry4_filtered$Total[routes_ferry4_filtered$mode == "FERRY"])
 
 
 # get potential
@@ -42,22 +47,18 @@ routes_ferry4_filtered = routes_ferry4_filtered %>% mutate(
 
 summary(routes_ferry4_filtered$cyc4)
 summary(routes_ferry4_filtered$new_cyc4) 
-sum(routes_ferry4_filtered$Bike) #21k
-sum(routes_ferry4_filtered$cyc4) #166k
-sum(routes_ferry4_filtered$new_cyc4) #149k
-sum(routes_ferry4_filtered$cyc10) #415k
-sum(routes_ferry4_filtered$new_cyc10) #396k
-
-
-routes_ferry4_filtered[1,]
-routes_ferry4_filtered[3000,]
+sum(routes_ferry4_filtered$Bike) #15k
+sum(routes_ferry4_filtered$cyc4) #114k
+sum(routes_ferry4_filtered$new_cyc4) #102k
+sum(routes_ferry4_filtered$cyc10) #286k
+sum(routes_ferry4_filtered$new_cyc10) #272k
 
 rnet_ferry4_overline = routes_ferry4_filtered %>%
   stplanr::overline(c("Bike", "Total", "new_cyc4", "cyc4", "new_cyc10", "cyc10"),
                     fun = "sum") %>% 
   mutate(Bikeper = Bike / Total)
 saveRDS(rnet_ferry4_overline, "routes_ferry4_overline.Rds")
-
+saveRDS(routes_ferry4_filtered, "routes_ferry4_preoverline.Rds")
 
 ## filter segments with less than X trips?
 summary(rnet_ferry4_overline$new_cyc4)
@@ -136,13 +137,18 @@ routes_ferry2_filtered = routes_r5r_100jit_lts2__ferry_elev %>%
   st_drop_geometry() %>%
   group_by(id) %>% 
   summarise(distance = distance)
-summary(routes_ferry2_filtered$distance) #90% less than 9000m in total
-routes_ferry2_filtered = routes_ferry2_filtered %>% filter(distance <= 9000)
+summary(routes_ferry2_filtered$distance) ##50% less than 4500m, 75% less than 7700m in total
+routes_ferry2_filtered = routes_ferry2_filtered %>% filter(distance <= 5000) #fazer até 5km para os cenarios 4 e 10
+
 
 routes_ferry2_filtered = routes_r5r_100jit_lts2__ferry_elev %>% filter(id %in% routes_ferry2_filtered$id)
 table(routes_ferry2_filtered$mode)
-# 36k trips by bike
-colnames(routes_ferry2_filtered)
+# BICYCLE   FERRY 
+# 24431    812 
+# 2.398k    69k
+# 2.329k (2  pernas da mesma viagem, first & last mile) 
+
+# sum(routes_ferry2_filtered$Total[routes_ferry2_filtered$mode == "BICYCLE"])
 
 
 # get potential
@@ -160,22 +166,19 @@ routes_ferry2_filtered = routes_ferry2_filtered %>% mutate(
 
 summary(routes_ferry2_filtered$cyc4)
 summary(routes_ferry2_filtered$new_cyc4) 
-sum(routes_ferry2_filtered$Bike) #19k
-sum(routes_ferry2_filtered$cyc4) #143k
-sum(routes_ferry2_filtered$new_cyc4) #127k
-sum(routes_ferry2_filtered$cyc10) #358k
-sum(routes_ferry2_filtered$new_cyc10) #340k
+sum(routes_ferry2_filtered$Bike) #13k
+sum(routes_ferry2_filtered$cyc4) #99k
+sum(routes_ferry2_filtered$new_cyc4) #88k
+sum(routes_ferry2_filtered$cyc10) #247k
+sum(routes_ferry2_filtered$new_cyc10) #235k
 
-
-routes_ferry2_filtered[1,]
-routes_ferry2_filtered[3000,]
 
 rnet_ferry2_overline = routes_ferry2_filtered %>%
   stplanr::overline(c("Bike", "Total", "new_cyc4", "cyc4", "new_cyc10", "cyc10"),
                     fun = "sum") %>% 
   mutate(Bikeper = Bike / Total)
 saveRDS(rnet_ferry2_overline, "routes_ferry2_overline.Rds")
-
+saveRDS(routes_ferry2_filtered, "routes_ferry2_preoverline.Rds")
 
 ## filter segments with less than X trips?
 summary(rnet_ferry2_overline$new_cyc4)
@@ -232,6 +235,236 @@ tm_rnet(rnet_ferry2_overline_morethan100_clean,
 saveRDS(rnet_ferry2_overline_morethan100_clean, "export/rnet_ferry2_overline_morethan100_clean.Rds")
 saveRDS(rnet_ferry2_overline_morethan10_clean, "export/rnet_ferry2_overline_morethan10_clean.Rds")
 # rnet_ferry2_overline_morethan100_clean = readRDS("export/rnet_ferry2_overline_morethan100_clean.Rds")
+
+
+# for ebikes ----------------------------------------------------------------------------------
+
+## LTS4
+# filter for electric bike
+table(routes_r5r_100jit_lts4__ferry_elev$mode)
+# BICYCLE   FERRY 
+# 54384    2011 
+summary(routes_r5r_100jit_lts4__ferry_elev$distance)
+
+routes_ferry4_ebike_filtered = routes_r5r_100jit_lts4__ferry_elev %>%
+  filter(mode == "BICYCLE") %>%
+  st_drop_geometry() %>%
+  group_by(id) %>% 
+  summarise(distance = distance)
+summary(routes_ferry4_ebike_filtered$distance) #50% less than 5000m, 75% less than 9000m in total
+routes_ferry4_ebike_filtered = routes_ferry4_ebike_filtered %>% filter(distance <= 10000) #fazer até 10km para os cenarios ebike
+
+
+routes_ferry4_ebike_filtered = routes_r5r_100jit_lts4__ferry_elev %>% filter(id %in% routes_ferry4_ebike_filtered$id)
+table(routes_ferry4_ebike_filtered$mode)
+# BICYCLE   FERRY 
+# 43388    2011 
+# 4.182k    162k
+
+# sum(routes_ferry4_ebike_filtered$Total[routes_ferry4_ebike_filtered$mode == "BICYCLE"])
+
+
+# get potential
+#cycling potential function
+ENMAC4 = 0.04 # 4%
+ENMAC10 = 0.10 # 10#
+routes_ferry4_ebike_filtered$Bikeper = routes_ferry4_ebike_filtered$Bike / routes_ferry4_ebike_filtered$Total
+
+routes_ferry4_ebike_filtered = routes_ferry4_ebike_filtered %>% mutate(
+  cyc4 = ifelse(Bikeper >= 4, Bike, ENMAC4 * Total),
+  new_cyc4 = ifelse(Bikeper >= ENMAC4, 0, cyc4 - Bike),
+  cyc10 = ifelse(Bikeper >= 10, Bike, ENMAC10 * Total),
+  new_cyc10 = ifelse(Bikeper >= ENMAC10, 0, cyc10 - Bike)
+)
+
+summary(routes_ferry4_ebike_filtered$cyc4)
+summary(routes_ferry4_ebike_filtered$new_cyc4) 
+sum(routes_ferry4_ebike_filtered$Bike) #22k
+sum(routes_ferry4_ebike_filtered$cyc4) #174k
+sum(routes_ferry4_ebike_filtered$new_cyc4) #156k
+sum(routes_ferry4_ebike_filtered$cyc10) #435k
+sum(routes_ferry4_ebike_filtered$new_cyc10) #414k
+
+rnet_ferry4_ebike_overline = routes_ferry4_ebike_filtered %>%
+  stplanr::overline(c("Bike", "Total", "new_cyc4", "cyc4", "new_cyc10", "cyc10"), #vale a pena manter ambos 4% 4 10% ?
+                    fun = "sum") %>% 
+  mutate(Bikeper = Bike / Total)
+saveRDS(rnet_ferry4_ebike_overline, "routes_ferry4_ebike_overline.Rds")
+saveRDS(routes_ferry4_ebike_filtered, "routes_ferry4_ebike_preoverline.Rds")
+
+## filter segments with less than X trips?
+summary(rnet_ferry4_ebike_overline$new_cyc4)
+
+rnet_ferry4_ebike_overline_morethan10 = rnet_ferry4_ebike_overline %>% filter(cyc4 >=10)
+rnet_ferry4_ebike_overline_morethan100 = rnet_ferry4_ebike_overline %>% filter(cyc4 >=100)
+
+summary(rnet_ferry4_ebike_overline_morethan10$new_cyc4)
+summary(rnet_ferry4_ebike_overline_morethan10$cyc4)
+summary(rnet_ferry4_ebike_overline_morethan100$new_cyc4)
+summary(rnet_ferry4_ebike_overline_morethan100$cyc4)
+
+#rnet clean to get rid of small pieces - 100
+rnet_ferry4_ebike_overline_morethan100_clean = rnet_ferry4_ebike_overline_morethan100 %>%
+  mutate(group = stplanr::rnet_group(rnet_ferry4_ebike_overline_morethan100, d = 100 ))
+summary(rnet_ferry4_ebike_overline_morethan100_clean$group)
+
+rnet_ferry4_ebike_overline_morethan100_group = rnet_ferry4_ebike_overline_morethan100_clean %>%
+  st_drop_geometry() %>% group_by(group) %>% summarise(count = n())
+summary(rnet_ferry4_ebike_overline_morethan100_group$count)
+group = rnet_ferry4_ebike_overline_morethan100_group %>% filter(count >=5) #discard networks with less than 5 semnets?
+groupp = group$group
+
+rnet_ferry4_ebike_overline_morethan100_clean = rnet_ferry4_ebike_overline_morethan100_clean %>%
+  filter(group %in% groupp) #more than 5 segments connected
+rm(rnet_ferry4_ebike_overline_morethan100_group)
+
+
+# #rnet clean to get rid of small pieces - 10
+# rnet_ferry4_ebike_overline_morethan10_clean = rnet_ferry4_ebike_overline_morethan10 %>%
+#   mutate(group = stplanr::rnet_group(rnet_ferry4_ebike_overline_morethan10, d = 100 ))
+# summary(rnet_ferry4_ebike_overline_morethan10_clean$group)
+# 
+# rnet_ferry4_ebike_overline_morethan10_group = rnet_ferry4_ebike_overline_morethan10_clean %>%
+#   st_drop_geometry() %>% group_by(group) %>% summarise(count = n())
+# summary(rnet_ferry4_ebike_overline_morethan10_group$count)
+# group = rnet_ferry4_ebike_overline_morethan10_group %>% filter(count >=5) #discard networks with less than 5 semnets?
+# groupp = group$group
+# 
+# rnet_ferry4_ebike_overline_morethan10_clean = rnet_ferry4_ebike_overline_morethan10_clean %>%
+#   filter(group %in% groupp) #more than 5 segments connected
+# rm(rnet_ferry4_ebike_overline_morethan10_group)
+
+
+library(biclar)
+tm_rnet(rnet_ferry4_ebike_overline_morethan100_clean,
+        lwd = "cyc4", #Baseline, ENMAC4, ENMAC10
+        # col = "Quietness",
+        palette = "-linear_yl_rd_bk", # "linear_yl_rd_bk" "johnson", "mako", "burg", "reds" - reds for fastest, mako for quietest
+        scale = 15,
+        lwd_multiplier = 15
+)
+
+saveRDS(rnet_ferry4_ebike_overline_morethan100_clean, "export/rnet_ferry4_ebike_overline_morethan100_clean.Rds")
+# saveRDS(rnet_ferry4_ebike_overline_morethan10_clean, "export/rnet_ferry4_ebike_overline_morethan10_clean.Rds")
+# rnet_ferry4_ebike_overline_morethan100_clean = readRDS("export/rnet_ferry4_ebike_overline_morethan100_clean.Rds")
+
+# Run also for LTS3, and mode egress = walk
+
+
+
+
+## LTS2
+# filter for ebike bike
+table(routes_r5r_100jit_lts2__ferry_elev$mode)
+# BICYCLE   FERRY 
+# 43731    899 
+summary(routes_r5r_100jit_lts2__ferry_elev$distance)
+
+
+routes_ferry2_ebike_filtered = routes_r5r_100jit_lts2__ferry_elev %>%
+  filter(mode == "BICYCLE") %>%
+  st_drop_geometry() %>%
+  group_by(id) %>% 
+  summarise(distance = distance)
+summary(routes_ferry2_ebike_filtered$distance) ##50% less than 4500m, 75% less than 7700m in total
+routes_ferry2_ebike_filtered = routes_ferry2_ebike_filtered %>% filter(distance <= 10000) #fazer até 5km para os cenarios 4 e 10
+
+
+routes_ferry2_ebike_filtered = routes_r5r_100jit_lts2__ferry_elev %>% filter(id %in% routes_ferry2_ebike_filtered$id)
+table(routes_ferry2_ebike_filtered$mode)
+# BICYCLE   FERRY 
+# 37674    899 
+# 3.679k    76k
+
+# sum(routes_ferry2_ebike_filtered$Total[routes_ferry2_ebike_filtered$mode == "FERRY"])
+
+
+# get potential
+#cycling potential function
+ENMAC4 = 0.04 # 4%
+ENMAC10 = 0.10 # 10#
+routes_ferry2_ebike_filtered$Bikeper = routes_ferry2_ebike_filtered$Bike / routes_ferry2_ebike_filtered$Total
+
+routes_ferry2_ebike_filtered = routes_ferry2_ebike_filtered %>% mutate(
+  cyc4 = ifelse(Bikeper >= 4, Bike, ENMAC4 * Total),
+  new_cyc4 = ifelse(Bikeper >= ENMAC4, 0, cyc4 - Bike),
+  cyc10 = ifelse(Bikeper >= 10, Bike, ENMAC10 * Total),
+  new_cyc10 = ifelse(Bikeper >= ENMAC10, 0, cyc10 - Bike)
+)
+
+summary(routes_ferry2_ebike_filtered$cyc4)
+summary(routes_ferry2_ebike_filtered$new_cyc4) 
+sum(routes_ferry2_ebike_filtered$Bike) #19k
+sum(routes_ferry2_ebike_filtered$cyc4) #150k
+sum(routes_ferry2_ebike_filtered$new_cyc4) #133k
+sum(routes_ferry2_ebike_filtered$cyc10) #375k
+sum(routes_ferry2_ebike_filtered$new_cyc10) #357k
+
+
+rnet_ferry2_ebike_overline = routes_ferry2_ebike_filtered %>%
+  stplanr::overline(c("Bike", "Total", "new_cyc4", "cyc4", "new_cyc10", "cyc10"),
+                    fun = "sum") %>% 
+  mutate(Bikeper = Bike / Total)
+saveRDS(rnet_ferry2_ebike_overline, "routes_ferry2_ebike_overline.Rds")
+saveRDS(routes_ferry2_ebike_filtered, "routes_ferry2_ebike_preoverline.Rds")
+
+## filter segments with less than X trips?
+summary(rnet_ferry2_ebike_overline$new_cyc4)
+
+rnet_ferry2_ebike_overline_morethan10 = rnet_ferry2_ebike_overline %>% filter(cyc4 >=10)
+rnet_ferry2_ebike_overline_morethan100 = rnet_ferry2_ebike_overline %>% filter(cyc4 >=100)
+
+summary(rnet_ferry2_ebike_overline_morethan10$new_cyc4)
+summary(rnet_ferry2_ebike_overline_morethan10$cyc4)
+summary(rnet_ferry2_ebike_overline_morethan100$new_cyc4)
+summary(rnet_ferry2_ebike_overline_morethan100$cyc4)
+
+#rnet clean to get rid of small pieces - 100
+rnet_ferry2_ebike_overline_morethan100_clean = rnet_ferry2_ebike_overline_morethan100 %>%
+  mutate(group = stplanr::rnet_group(rnet_ferry2_ebike_overline_morethan100, d = 100 ))
+summary(rnet_ferry2_ebike_overline_morethan100_clean$group)
+
+rnet_ferry2_ebike_overline_morethan100_group = rnet_ferry2_ebike_overline_morethan100_clean %>%
+  st_drop_geometry() %>% group_by(group) %>% summarise(count = n())
+summary(rnet_ferry2_ebike_overline_morethan100_group$count)
+group = rnet_ferry2_ebike_overline_morethan100_group %>% filter(count >=5) #discard networks with less than 5 semnets?
+groupp = group$group
+
+rnet_ferry2_ebike_overline_morethan100_clean = rnet_ferry2_ebike_overline_morethan100_clean %>%
+  filter(group %in% groupp) #more than 5 segments connected
+rm(rnet_ferry2_ebike_overline_morethan100_group, group)
+
+
+# # #rnet clean to get rid of small pieces - 10
+# rnet_ferry2_ebike_overline_morethan10_clean = rnet_ferry2_ebike_overline_morethan10 %>%
+#   mutate(group = stplanr::rnet_group(rnet_ferry2_ebike_overline_morethan10, d = 100 ))
+# summary(rnet_ferry2_ebike_overline_morethan10_clean$group)
+# 
+# rnet_ferry2_ebike_overline_morethan10_group = rnet_ferry2_ebike_overline_morethan10_clean %>%
+#   st_drop_geometry() %>% group_by(group) %>% summarise(count = n())
+# summary(rnet_ferry2_ebike_overline_morethan10_group$count)
+# group = rnet_ferry2_ebike_overline_morethan10_group %>% filter(count >=3) #discard networks with less than 3 semnets?
+# groupp = group$group
+# 
+# rnet_ferry2_ebike_overline_morethan10_clean = rnet_ferry2_ebike_overline_morethan10_clean %>%
+#   filter(group %in% groupp) #more than 5 segments connected
+# rm(rnet_ferry2_ebike_overline_morethan10_group, group)
+
+
+library(biclar)
+tm_rnet(rnet_ferry2_ebike_overline_morethan100_clean,
+        lwd = "Bike", #Baseline, ENMAC4, ENMAC10
+        # col = "Quietness",
+        palette = "-reds", # "linear_yl_rd_bk" "johnson", "mako", "burg", "reds" - reds for fastest, mako for quietest
+        scale = 15,
+        lwd_multiplier = 15
+)
+
+saveRDS(rnet_ferry2_ebike_overline_morethan100_clean, "export/rnet_ferry2_ebike_overline_morethan100_clean.Rds")
+saveRDS(rnet_ferry2_ebike_overline_morethan10_clean, "export/rnet_ferry2_ebike_overline_morethan10_clean.Rds")
+# rnet_ferry2_ebike_overline_morethan100_clean = readRDS("export/rnet_ferry2_ebike_overline_morethan100_clean.Rds")
+
+
 
 
 # for intermodality ---------------------------------------------------------------------------
