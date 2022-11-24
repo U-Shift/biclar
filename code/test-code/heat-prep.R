7f4a1ca98b6841471bee52adbe259dd07f419ce1
temospena <temospena@gmail.com>
2022-11-24 12:36:07 +0000

    add source for locations id

--- code/test-code/heat-prep.R
+++ code/test-code/heat-prep.R
@@ -1,6 +1,16 @@
 # prepare tables for HEAT SE estimations
 
 
+# locations id
+library(readr)
+HEAT_all_locations_data <- read_csv("D:/GIS/TML/HEAT_heatr_api/all_locations_data.csv") #data from https://github.com/HEAT-WHO/HEAT_data/blob/main/data/heatr/locations_data/all_locations_data.csv
+HEAT_all_locations_data = HEAT_all_locations_data %>% filter(country_location_name == "Portugal")
+HEAT_all_locations_data = HEAT_all_locations_data %>% filter(sub1_location_name %in% c("Distrito de Lisboa", "Distrito de Setúbal"))
+HEAT_all_locations_data = HEAT_all_locations_data %>% filter(is.na(city_location_id))
+saveRDS(HEAT_all_locations_data, "HEAT/HEAT_locations_data_AML.Rds")
+
+
+
 # PM25 por municipio, incluindo AML
 PM25 = readxl::read_excel("export2/Scenarios_Routing.xlsx", sheet = "PM25")
 PM25$PM25 = round(PM25$PM25, 2)
