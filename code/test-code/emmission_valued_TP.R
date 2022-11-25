6a0a25a0ccb64157cf106cc7e7ab33f869c1ff82
temospena <temospena@gmail.com>
2022-11-25 18:26:04 +0000

    add replaced TP emissions

--- code/test-code/emmission_valued_TP.R
+++ code/test-code/emmission_valued_TP.R
@@ -4,3 +4,25 @@
 CAR_TP_TRANSF_results <- readxl::read_excel("HEAT/CAR_TP_TRANSF_results.xlsm", sheet = "Values", range = "A1:N5")
 
 # join with HEAT AML redux
+
+HEAT_aml_results = HEAT_aml_redux %>%
+  left_join(CAR_TP_TRANSF_results %>% select(Code, CO2eq_TP, CO_PT, PM10_PT, NOx_PT, VOC_PT, money_emissions_eur))
+
+
+saveRDS(HEAT_aml_results, "HEAT/HEAT_aml_results.Rds")
+
+
+
+
+
+# verificar -----------------------------------------------------------------------------------
+
+# sum(HEAT_bike$distance*HEAT_bike$new_cyc)
+# dist_bike_0km= sum(HEAT_bike$distance*HEAT_bike$new_cyc)/1000 #148407.1 km/dia
+# 
+# HEAT$CO2eq #5919 ton / ano
+# # 5919*1000000/365 = 16216438 g/dia
+# 
+# 16216438 / dist_bike_0km = 109.27 g/km 
+# 
+# #está certo!
