1ba04c2d8f992ecc77e7b91773b57915af3173f7
temospena <temospena@gmail.com>
2022-12-03 12:26:34 +0000

    progress with maps

--- code/biclarwww/build.R
+++ code/biclarwww/build.R
@@ -24,7 +24,7 @@ for (i in municipios){
   pub_folder = paste0("biclarwww/", i) #this needs to be updated for each municipio
   dir.create(pub_folder) #comment this one??
   rmarkdown::render(input = "code/biclarwww/municipio_index.Rmd", output_dir = pub_folder, output_file = "index.html")
-  # rmarkdown::render(input = "county-stats.Rmd", output_dir = pub_folder, output_file = "county-stats.html")
+  # rmarkdown::render(input = "stats.Rmd", output_dir = pub_folder, output_file = "stats.html")
   # rmarkdown::render(input = "baseline.Rmd", output_dir = pub_folder, output_file = "baseline.html")
   # rmarkdown::render(input = "route-types.Rmd", output_dir = pub_folder, output_file = "route-types.html")
   # rmarkdown::render(input = "downloads.Rmd", output_dir = pub_folder, output_file = "downloads.html")
