2eda431d113c3ba66f82fc74fee6ca82561eaf12
temospena <temospena@gmail.com>
2022-12-04 14:50:37 +0000

    solve Vila Franca Xira

--- code/biclarwww/build.R
+++ code/biclarwww/build.R
@@ -21,7 +21,11 @@ i = "barreiro"
 
 for (i in municipios){
   
-  pub_folder = paste0("biclarwww/", i) #this needs to be updated for each municipio
+  if (i = "vila franca de xira"){
+    pub_folder = paste0("biclarwww/", "vfxira")
+      } else {
+    pub_folder = paste0("biclarwww/", i) #this needs to be updated for each municipio
+  }
   # dir.create(pub_folder) #comment this one??
   rmarkdown::render(input = "code/biclarwww/municipio_index.Rmd", output_dir = pub_folder, output_file = "index.html")
   # rmarkdown::render(input = "stats.Rmd", output_dir = pub_folder, output_file = "stats.html")
