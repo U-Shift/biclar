feb263b702cc7ca7707e0328d34e6be0c07be648
RFlx <temospena@gmail.com>
2023-04-11 14:22:29 +0100

    Update heat-prep.R
    tidyup #38

--- code/heat-prep.R
+++ code/heat-prep.R
@@ -9,9 +9,10 @@ HEAT_all_locations_data = HEAT_all_locations_data %>% filter(country_location_na
 HEAT_all_locations_data = HEAT_all_locations_data %>% filter(sub1_location_name %in% c("Distrito de Lisboa", "Distrito de Setúbal"))
 HEAT_all_locations_data = HEAT_all_locations_data %>% filter(is.na(city_location_id))
 # saveRDS(HEAT_all_locations_data, "HEAT/HEAT_locations_data_AML.Rds")
-# match location id with the 18 different locations in the spreadsheet, and add mean in missing ones and in AML (region).
+# match location id with the 18 different locations (municipalities) in the spreadsheet, and add mean in missing ones and in AML (region).
 
-# PM25 por municipio, incluindo AML
-PM25 = readxl::read_excel("export2/Scenarios_Routing.xlsx", sheet = "PM25")
+# PM25 by municipality, incluindo AML
+PM25 = openxlsx::read.xlsx("https://github.com/U-Shift/biclar/releases/download/0.0.1/Scenarios_Routing.xlsx", sheet = "PM25")
 PM25$PM25 = round(PM25$PM25, 2)
-PM25$location_id = as.character(PM25$location_id)
+PM25$location_id = as.character(PM25$location_id) # needed to use as input in HaaS
+
